d27ff05c1690de357f66744425e190fd3680180a
ignoxx <ignoxx@yahoo.de>
2024-01-08 01:47:19 +0100

    update: webhook verification function

--- webhooks.go
+++ webhooks.go
@@ -12,11 +12,24 @@ import (
 	"strings"
 )
 
-// Parse the webhook body into an InvoiceEvent
-func ParseWebhook(r *http.Request) (*InvoiceEvent, error) {
+// Verify the webhook signatura and parse it into *InvoiceEvent
+func VerifyWebhook(r *http.Request, webhookSecret string) (*InvoiceEvent, error) {
+	var messageMAC = []byte(strings.TrimPrefix(r.Header.Get("BTCPay-Sig"), "sha256="))
+	if len(messageMAC) == 0 {
+		return nil, errors.New("BTCPay-Sig header missing")
+	}
+
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		return nil, err
+	}
+
+	var mac = hmac.New(sha256.New, []byte(webhookSecret))
+	mac.Write(body)
+
+	var expectedMAC = []byte(hex.EncodeToString(mac.Sum(nil)))
+	if !hmac.Equal(messageMAC, expectedMAC) {
+		return nil, fmt.Errorf("HMAC mismatch, got %s, want %s", messageMAC, expectedMAC)
 	}
 
 	var event = &InvoiceEvent{}
@@ -24,28 +37,5 @@ func ParseWebhook(r *http.Request) (*InvoiceEvent, error) {
 		return nil, err
 	}
 
-	return event, err
-}
-
-// Verify the webhook signature
-func (c *Client) VerifyWebhook(r *http.Request) error {
-	var messageMAC = []byte(strings.TrimPrefix(r.Header.Get("BTCPay-Sig"), "sha256="))
-	if len(messageMAC) == 0 {
-		return errors.New("BTCPay-Sig header missing")
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-
-	var mac = hmac.New(sha256.New, []byte(c.WebhookSecret))
-	mac.Write(body)
-
-	var expectedMAC = []byte(hex.EncodeToString(mac.Sum(nil)))
-	if !hmac.Equal(messageMAC, expectedMAC) {
-		return fmt.Errorf("HMAC mismatch, got %s, want %s", messageMAC, expectedMAC)
-	}
-
-	return nil
+	return event, nil
 }
